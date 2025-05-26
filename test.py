b201439897f44e606720a5b6b60d51a6825715d7
sirisha-fulara <166629183+sirisha-fulara@users.noreply.github.com>
2025-05-26 20:10:15 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,39 +2,39 @@ from flask import Flask, jsonify, request, render_template
 from flask_cors import CORS
 import sys
 import os
-import ctypes
-python_dll = os.path.join(sys.exec_prefix, 'python313.dll')
+#import ctypes
+#python_dll = os.path.join(sys.exec_prefix, 'python313.dll')
 
 # Load it remove in git 
-try:
-    ctypes.WinDLL(python_dll) 
-    print("✅ Python DLL loaded successfully from:", python_dll)
-except OSError as e:
-    print("❌ Failed to load Python DLL:", e)
+#try:
+#    ctypes.WinDLL(python_dll) 
+#    print("✅ Python DLL loaded successfully from:", python_dll)
+#except OSError as e:
+#    print("❌ Failed to load Python DLL:", e)
 # Add the root folder to sys.path dynamically (3 levels up from this file)
 #linus code for render 
-# import importlib.util
-# # Set correct relative path to .so file
-# cpp_backend_dir = os.path.abspath("cpp_backend")
-# so_path = os.path.join(cpp_backend_dir, "route_optimizer.cpython-312-x86_64-linux-gnu.so")
+import importlib.util
+# Set correct relative path to .so file
+cpp_backend_dir = os.path.abspath("cpp_backend")
+so_path = os.path.join(cpp_backend_dir, "route_optimizer.cpython-312-x86_64-linux-gnu.so")
 
 # # Add cpp_backend to sys.path if needed
-# if cpp_backend_dir not in sys.path:
-#     sys.path.insert(0, cpp_backend_dir)
+if cpp_backend_dir not in sys.path:
+    sys.path.insert(0, cpp_backend_dir)
 
 # # Dynamically load the shared object (.so)
-# spec = importlib.util.spec_from_file_location("route_optimizer", so_path)
-# route_optimizer = importlib.util.module_from_spec(spec)
-# spec.loader.exec_module(route_optimizer)
+spec = importlib.util.spec_from_file_location("route_optimizer", so_path)
+route_optimizer = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(route_optimizer)
 
 print("✅ C++ backend (.so) module loaded successfully.") 
 
 # Setup for compiled C++ module
-module_path = os.path.abspath("cpp_backend/build/release")
-if module_path not in sys.path:
-    sys.path.insert(0, module_path)
+#module_path = os.path.abspath("cpp_backend/build/release")
+#if module_path not in sys.path:
+#    sys.path.insert(0, module_path)
 
-import route_optimizer
+#import route_optimizer
 
 app = Flask(__name__)
 CORS(app)
@@ -74,4 +74,4 @@ def reconstruct_path(parent, src, dest):
     return path[::-1] if path and path[-1] == src else []
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
